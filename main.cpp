b8840f293db5de5e585a9b77b8e5e40774f62003
Ivania Donoso <indonoso@uc.cl>
2016-05-28 21:07:57 -0400

    Output to console deleted. It is not used

--- main.cpp
+++ main.cpp
@@ -120,7 +120,7 @@ int main(int argc, char** argv)
       cerr << " (please note that the input should be one-sentence-per-line)." << endl;
     }
     string postagged = bidir_postag(line, vme, vme_chunking, dont_tokenize);
-    cout << postagged << endl;
+    // cout << postagged << endl;
     //Added By Ivania
     fs << postagged << endl;
     //Added By Ivania
